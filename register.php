60e63cdeeb6d842abcafa3d4e9490968bdc6c97c
KrPrlsk <63001443+KrPrlsk@users.noreply.github.com>
2020-06-15 13:12:04 +0300

    Add files via upload

---
+++ register.php
@@ -0,0 +1,41 @@
+<html>
+<body>
+    <form method="post">
+        <table>
+            <tr><td>Username:</td><td><input type="text" name="reg_uname" value="UserName" /></td></tr>
+            <tr><td>Password:</td><td><input type="password" name="reg_password" value="Password" /></td></tr>
+            <tr><td>
+                <input type="submit" name="my_form_submit_button" value="Just Register"/>
+            </td><td>
+
+            </td></tr>
+        </table>
+    </form>
+
+    <form method ="get" action="index.php">
+        <input type="submit" value="Try to login" />
+    </form>
+
+    <?php
+        include 'db_connection.php';
+        if (isset($_POST['reg_uname'])) { //если запись в форме существует
+            include 'db.php'; //подключаем файл
+            createUser($_POST['reg_uname'],$_POST['reg_password']);
+        }
+        $query = "select * from users"; //выбираем все из пользователей
+        $res = mysql_query($query); //переменная с запросом к БД
+        while($dsatz = mysql_fetch_assoc($res)) //выводит ассоциативный массив
+        {
+            echo "<table>";
+            echo "<tr>";
+            echo "<td>" . $dsatz["id"] . "</td>" .
+            "<td>" . $dsatz["username"] . "</td>" .
+            "<td>" . $dsatz["passwordHash"] . "</td>";
+            echo "</tr>";
+            echo "</table>";
+        }
+    ?>
+
+
+</body>
+</html>
